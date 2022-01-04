41fb58e1bbce77208edb9dccd52ca57dc393cf8c
Thanuja Lakmal <thanuja@wso2.com>
2022-01-04 21:53:27 +0530

    Revert "updated"

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ const Header = (props: Props) => {
       <div className="flex flex-col sm:flex-row sm:items-center pr-4 justify-between max-w-[1440px] m-auto">
         <Link to={"/"} className="flex px-8 py-3 items-center gap-3">
           <img src={Logo} className="w-10 h-10" />
-          <span className="text-[#5f6368] text-xl">User Manager PRO </span>
+          <span className="text-[#5f6368] text-xl">User Manager PRO</span>
         </Link>
         <button
           onClick={() => navigate("/add")}
