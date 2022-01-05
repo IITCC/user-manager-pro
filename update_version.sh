ecb9c4d2df8e99942a4538e17d97669a39941683
thanujalk <thanuja@wso2.com>
2022-01-05 18:27:40 +0530

    add update_version.sh

---
+++ update_version.sh
@@ -0,0 +1,24 @@
+echo "Update web app version"
+
+GIT_USERNAME=$1
+GIT_TOKEN=$2
+WORK_DIR=$3
+TAG=$4
+GIT_EMAIL="thanuja.rashmi.tv@gmail.com"
+
+mkdir helm_user_mgt
+
+# Clone the repo to push changes to main branch.
+git clone https://"${GIT_USERNAME}":"${GIT_TOKEN}"@github.com/IITCC/helm-user-mgt.git helm_user_mgt
+git -C helm_user_mgt config user.email "${GIT_EMAIL}"
+git -C helm_user_mgt config user.name "${GIT_USERNAME}"
+
+TAG_LINE=$(grep -w 'tag' "${WORK_DIR}"/helm_user_mgt/helm-user-mgt/values.yaml | sed 's/ *//')
+sed -i 's|'"${TAG_LINE}"'|tag: '"'${TAG}'"'|' "${WORK_DIR}"/helm_user_mgt/helm-user-mgt/values.yaml
+
+# Push new values.yaml.
+git -C helm_user_mgt add "${WORK_DIR}"/helm_user_mgt/helm-user-mgt/values.yaml
+git -C helm_user_mgt commit -m "Update user mgt app version to - ${TAG}"
+git -C helm_user_mgt push origin main
+
+echo "Update web app version done"
